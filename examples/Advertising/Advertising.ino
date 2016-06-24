58980926a233a2ea3c15d4d472132c08085da184
Shimakage <shimakage@gclue.jp>
2016-06-24 18:34:28 +0900

    modified Advertising examples

--- examples/Advertising/Advertising.ino
+++ examples/Advertising/Advertising.ino
@@ -24,20 +24,28 @@ void setup()
   
   if(faboBLE.setAdvParameters()){
     Serial.println("Success:setAdvParams()");
-    if(faboBLE.setMode()){
-      Serial.println("Success:setMode()");
       byte uuid[16] = {0xcb,0x86,0xbc,0x31,0x05,0xbd,0x40,0xcc,0x90,0x3d,0x1c,0x9b,0xd1,0x3d,0x96,0x6b};
       byte major[2] = {0x00,0x71};
       byte minor[2] = {0x00,0x71};
       faboBLE.setBeaconUuid(uuid);
       faboBLE.setBeaconMajor(major);
       faboBLE.setBeaconMinor(minor);
-    } else {
-      Serial.println("Failed:setMode()");
+
+    if(faboBLE.sendBeacon()){
+      Serial.println("Success:sendBeacon()");
+    } else{
+      Serial.println("Failed:sendBeacon");
     }
   } else {
     Serial.println("Failed:setAdvParams()"); 
   }
+
+  if(faboBLE.setMode()){
+    Serial.println("Success:Start Beacon advertising"); 
+  } else {
+    Serial.println("Failed:Start Beacon advertising"); 
+  }
+
 }
 
 void loop()
@@ -48,8 +56,8 @@ void loop()
   // ボタン押下判定
   if (buttonState == HIGH && isFirst == false) {
       isFirst == true;
-      if(!faboBLE.isAdvertising()){     
-        if(faboBLE.sendBeacon()){
+      if(!faboBLE.isAdvertising()){
+        if(faboBLE.setMode()){
           Serial.println("Success:Start Beacon advertising"); 
         } else {
           Serial.println("Failed:Start Beacon advertising"); 
